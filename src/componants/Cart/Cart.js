27b5a094efca6e4fc6b7ff0eea28cd91b81c83bb
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-26 15:20:05 +0600

    order summary calculation done easily in cart.js

--- src/componants/Cart/Cart.js
+++ src/componants/Cart/Cart.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
+    // tota price add OMG very easy wayyyyy
+    const { cart } = props;
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    // json.data file tax na thakay amra taxt adde kore dilm
+    const tax = parseFloat((total * 0.1).toFixed(2));
+
+    // grand total
+    const grandTotal = (total + shipping + tax).toFixed(2);
+
+
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary</h4>
-            <h5>Selected items: {props.cart.length}</h5>
+            <p>Selected items: {props.cart.length}</p>
+            <p>Total price:  ${total}</p>
+            <p>Total Shipping:  ${shipping}</p>
+            <p>Tax/VAT: ${tax}</p>
+            <h5>Grand Total: ${grandTotal}</h5>
         </div>
     );
 };
 
 export default Cart;
+
+//    // tota price add OMG very easy wayyyyy
+//    const { cart } = props;
+//    let total = 0;
+//    let shipping = 0;
+//    for (const product of cart) {
+//        total = total + product.price;
+//        shipping = shipping + product.shipping;
+//    }
+//    // json.data file tax na thakay amra taxt adde kore dilm
+//    const tax = parseFloat((total * 0.1).toFixed(2));
+
+//    // grand total
+//    const grandTotal = (total + shipping + tax).toFixed(2);
